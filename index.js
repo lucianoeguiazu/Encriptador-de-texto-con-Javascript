57e2cbef9b9efbbe40bc18c13a0914982db36ae0
Luciano Eguiazu <eguiazu.luciano@gmail.com>
2023-01-09 11:19:36 -0300

    desafio alura

---
+++ index.js
@@ -0,0 +1,39 @@
+function encriptar(){
+	var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+	var textoCifrado = texto.replace(/e/igm, "enter");
+	var textoCifrado = textoCifrado.replace(/o/igm, "over");
+	var textoCifrado = textoCifrado.replace(/i/igm, "imes");
+	var textoCifrado = textoCifrado.replace(/a/igm, "ai");
+	var textoCifrado = textoCifrado.replace(/u/igm, "ufat");
+
+	document.getElementById("imagen").style.display = "none";
+	document.getElementById("h2msg").style.display = "none";
+	document.getElementById("txt").innerHTML = textoCifrado
+	document.getElementById("buttonCopiar").style.display = "show";
+	document.getElementById("buttonCopiar").style.display = "inherit";
+}
+
+function desencriptar(){
+	var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+	var textoCifrado = texto.replace(/enter/igm, "e");
+	var textoCifrado = textoCifrado.replace(/over/igm, "o");
+	var textoCifrado = textoCifrado.replace(/imes/igm, "i");
+	var textoCifrado = textoCifrado.replace(/ai/igm, "a");
+	var textoCifrado = textoCifrado.replace(/ufat/igm, "u");
+
+	document.getElementById("imagen").style.display = "none";
+	document.getElementById("h2msg").style.display = "none";
+	document.getElementById("txt").innerHTML = textoCifrado
+	document.getElementById("buttonCopiar").style.display = "show";
+	document.getElementById("buttonCopiar").style.display = "inherit";
+}
+
+function copiar(){
+	var contenido = document.querySelector("#txt");
+	contenido.select();
+	document.execCommand("cut")
+	alert("Su mensaje fue copiado")
+}
+
